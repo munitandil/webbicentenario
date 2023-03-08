b09527b54392ffa02763e6c4e826248938173118
teresal <teresalemble@hotmail.com>
2023-03-08 09:57:58 -0300

    ecosys

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,9 +3,9 @@ module.exports = {
         name: 'PaginaBicentenarioTandil',
         exec_mode: 'cluster',
         watch: true,
-        instances: 1, // Or a number of instances
+        instances: 2, // Or a number of instances
         script: './node_modules/nuxt/bin/nuxt.js',
-        max_memory_restart: "400M",
+        max_memory_restart: "1G",
         args: 'start'
     }]
 }
